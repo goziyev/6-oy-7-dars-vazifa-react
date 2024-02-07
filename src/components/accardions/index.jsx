a9a46ae66669227bdcb5a94cc269533dd0d8ca5b
goziyev <goziyevmuhammadgozi@gmail.com>
2024-02-07 13:50:02 +0500

    accordion task completed

--- src/components/accardions/index.jsx
+++ src/components/accardions/index.jsx
@@ -1,8 +1,69 @@
-import style from './index.module.css'
+import { useState } from "react";
+import style from "./index.module.css";
 export default function Accordion() {
+  const [active, setActive] = useState(false);
+
+  function handleCLick(e) {
+    setActive(!active);
+    console.log(e);
+  }
+const array = [
+  {
+    title:"sao"
+  },
+  {
+    title:"sao"
+  },
+  {
+    title:"sao"
+  },
+  {
+    title:"sao"
+  },
+  {
+    title:"sao"
+  },
+  {
+    title:"sao"
+  },
+]
   return (
     <>
-      <div>Accordion</div>
+      <div className={style.accardion}>
+      
+
+       {array.map((el,index) => {
+         return (
+          <div key={index} className={style.item}>
+          <p onClick={handleCLick} className={style.accardionTitle}>
+            Accordion<span>{active ? "-" : "+"}</span>
+          </p>
+          <p className={active ? style.active : style.noActive}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
+            distinctio, molestiae id enim odit cum itaque sed quam suscipit
+            perferendis magnam expedita vero, beatae illum.
+          </p>
+        </div>
+         )
+       })}
+       
+       
+       
+       
+       
+       
+       
+        {/* <div className={style.item}>
+          <p onClick={handleCLick} className={style.accardionTitle}>
+            Accordion<span>{active ? "-" : "+"}</span>
+          </p>
+          <p className={active ? style.active : style.noActive}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
+            distinctio, molestiae id enim odit cum itaque sed quam suscipit
+            perferendis magnam expedita vero, beatae illum.
+          </p>
+        </div> */}
+      </div>
     </>
   );
 }
