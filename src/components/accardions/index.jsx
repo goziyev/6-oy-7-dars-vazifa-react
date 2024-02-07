2919f32d9fd9aa550240f8b74ae114d83bb4e07e
goziyev <goziyevmuhammadgozi@gmail.com>
2024-02-07 15:56:54 +0500

    task completed

--- src/components/accardions/index.jsx
+++ src/components/accardions/index.jsx
@@ -1,46 +1,28 @@
+// Accordions.js
 import { useState } from "react";
 import AccordionCard from "../accordion";
 import style from "./index.module.css";
-export default function Accordions() {
 
-const array = [
-  {
-    title:"sao"
-  },
-  {
-    title:"sao"
-  },
-  {
-    title:"sao"
-  }
-]
+export default function Accordions() {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const array = [
+    { title: "sao" },
+    { title: "sao" },
+    { title: "sao" }
+  ];
+
   return (
     <>
       <div className={style.accardion}>
-      
-
-       {array.map((el,index) => {
-         return (
-          <AccordionCard  index={index} />
-         )
-       })}
-       
-       
-       
-       
-       
-       
-       
-        {/* <div className={style.item}>
-          <p onClick={handleCLick} className={style.accardionTitle}>
-            Accordion<span>{active ? "-" : "+"}</span>
-          </p>
-          <p className={active ? style.active : style.noActive}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
-            distinctio, molestiae id enim odit cum itaque sed quam suscipit
-            perferendis magnam expedita vero, beatae illum.
-          </p>
-        </div> */}
+        {array.map((el, index) => (
+          <AccordionCard
+            key={index}
+            index={index}
+            activeIndex={activeIndex}
+            setActiveIndex={setActiveIndex}
+          />
+        ))}
       </div>
     </>
   );
--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,18 +1,20 @@
+// AccordionCard.js
 import style from "../accardions/index.module.css";
 import { useState } from "react";
-export default function AccordionCard({ handleCLick, index }) {
-  const [active, setActive] = useState(false);
 
-  function handleCLick(e) {
-    setActive(!active);
-    console.log(e);
+export default function AccordionCard({ index, activeIndex, setActiveIndex }) {
+  const isActive = index === activeIndex;
+
+  function handleCLick() {
+    setActiveIndex(isActive ? null : index);
   }
+
   return (
     <div key={index} className={style.item}>
       <p onClick={handleCLick} className={style.accardionTitle}>
-        Accordion<span>{active ? "-" : "+"}</span>
+        Accordion<span>{isActive ? "-" : "+"}</span>
       </p>
-      <p className={active ? style.active : style.noActive}>
+      <p className={isActive ? style.active : style.noActive}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
         distinctio, molestiae id enim odit cum itaque sed quam suscipit
         perferendis magnam expedita vero, beatae illum.
